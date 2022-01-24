96a8ba790b4ef02ae1bdea880dc453ff0afdc854
roof <roof@odds.team>
2022-01-24 08:08:53 +0700

    Add test suite

---
+++ tennis_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from tennis import TennisGame
+
+from tennis_unittest import test_cases, play_game
+
+class TestTennis:
+
+    @pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
+    def test_get_score(self, p1Points, p2Points, score, p1Name, p2Name):
+        game = play_game(p1Points, p2Points, p1Name, p2Name)
+        assert score == game.print_score()
